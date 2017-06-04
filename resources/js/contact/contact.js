57d45c7c22bce332a2f50dff7e8cc0a8370b989e
Tom Lindley <tom@tlkr.co.uk>
2017-06-04 13:28:50 +0100

    Fixed bug with recaptcha not working correctly when dynamically drawing contact content

--- resources/js/contact/contact.js
+++ resources/js/contact/contact.js
@@ -5,12 +5,26 @@ new Vue({
             event.preventDefault();
             this.submit();
         });
+        this.setupRecaptcha();
     },
     data: {
         submitting: true,
-        submitted: false
+        submitted: false,
+        recaptchaInterval: null,
     },
     methods: {
+        setupRecaptcha: function() {
+            if (window.grecaptcha !== undefined) {
+                window.grecaptcha.render(this.$refs.captcha, {
+                    'sitekey' : this.$refs.captcha.dataset.sitekey
+                });
+                window.clearInterval(this.recaptchaInterval);
+            } else {
+                setTimeout(() => {
+                    this.setupRecaptcha();
+                }, 100);
+            }
+        },
         submit: function() {
             var formData = new FormData(this.$refs.form);
             this.submitting = true;
